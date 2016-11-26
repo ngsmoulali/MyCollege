9d554d21ec6064d17d282da5fb418e57f0cf0c72
ganga syed moulali nadendla <ngsmoulali@gmail.com>
2016-11-26 15:51:54 +0530

    AddedVIewpager
    created profileActivity for college profile and added viewpager

---
+++ app/src/main/java/ngsm/com/mycollege/ProfileActivity.java
@@ -0,0 +1,47 @@
+package ngsm.com.mycollege;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class ProfileActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    SwipeAdapter adapter;
+    int count = 0;
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        viewPager=(ViewPager)findViewById(R.id.vpProfile);
+        adapter=new SwipeAdapter(this);
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
+
+        // Timer for auto sliding
+        timer  = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(count<=3){
+                            viewPager.setCurrentItem(count);
+                            count++;
+                        }else{
+                            count = 0;
+                            viewPager.setCurrentItem(count);
+                        }
+                    }
+                });
+            }
+        }, 500, 3000);
+
+    }
+}
